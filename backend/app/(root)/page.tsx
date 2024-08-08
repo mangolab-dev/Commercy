192660eae50822acd60cdd49575885a89d60b923
Kauê Luí <ofckaue@gmail.com>
2024-08-08 20:08:41 -0300

    Arquivos iniciais do projeto usando a autenticação de usuário com o Clerk

---
+++ backend/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1> Olá, Administrador</h1>
+    <Button>Clique aqui</Button>
+    </>
+  );
+}
